ae8cf75d4493172cf79c564682906703cf85ebf1
Anthony <anthonyhermanto1@gmail.com>
2024-01-24 19:32:23 +0800

    iniital commit

---
+++ pages/index.js
@@ -0,0 +1,12 @@
+import NavBar from "@/components/NavBar"
+
+
+export default function Page() {
+    return (
+        <div>
+            <NavBar />
+            <p>hello world</p>
+        </div>
+    )
+
+}
